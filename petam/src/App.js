d2f9dd35739af9603a348d6af4912101d376f393
joo-ju <rlawn97@gmail.com>
2021-04-14 12:29:27 +0900

    make  Components & Pages     Components : Topbar, PostList, test     Pages : main
    make&edit  style.css

--- petam/src/App.js
+++ petam/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import './App.css';
+import TopBar from "./Components/Topbar.js";
+import PostList from "./Components/PostList.js";
+import Test from "./Components/test";
+export default class App extends Component {
+  
+  // function App() {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Route path="*" component={TopBar} />
+          <Route path="/postlist" component={PostList} />
+          <Route path="/main" component={Test} />
+          {/* <Route path="*" component={LeftSideBar} />
+        <Route path="/Reservation" component={Reservation} />
+        <Route path="/NewCustomer" component={NewCustomer} />
+        <Route path="/CustomerList" component={CustomerList} />
+        <Route path="/NewReservation" component={NewReservation} /> */}
+          
+        </Router>
+   
+      </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      //   <div className="App">
+      //     <header className="App-header">
+      //       <img src={logo} className="App-logo" alt="logo" />
+      //       <p>
+      //         Edit <code>src/App.js</code> and save to reload.
+      //       </p>
+      //       <a
+      //         className="App-link"
+      //         href="https://reactjs.org"
+      //         target="_blank"
+      //         rel="noopener noreferrer"
+      //       >
+      //         Learn React
+      //       </a>
+      //     </header>
+      //   </div>
+    );
+  }
 }
 
-export default App;
+// export default App;
---
+++ petam/src/Components/Topbar.js
@@ -0,0 +1,34 @@
+import '../style.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import React from 'react';
+// import ReactDOM from "react-dom";
+// import App from "../App.js";
+
+export default class TopBar extends Component {
+  render = () => (
+      <div>
+          {/* nav태그의 mb-3은 content와 간격 띄우기 */}
+      <nav class="mb-3 navbar navbar-expand navbar-light bg-primary topbar  static-top shadow">
+        <div className="col-9">
+          <Link to="/main">
+            <h4 className="bg-primary m-auto text-2140C text-center p-3 ">
+              Petam
+              {/* <img src={imgUser} className="w-25 h-25"></img> */}
+            </h4>
+          </Link>
+        </div>
+        <div className="  col-3">
+          <button className=" btn-light btn col-5 m-auto text-2140C text-center p-2 m-1">
+            Sign In
+          </button>
+          <button className=" btn-secondary btn col-5 m-auto text-white text-center float-right p-2 m-1">
+            Sign Up
+          </button>
+        </div>
+
+        {/* <!-- Topbar Navbar --> */}
+      </nav> 
+    </div>
+  );
+}
