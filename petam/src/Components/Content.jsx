589c4c85529a4490c568d45fb4dd1cec9e5e8da3
joo-ju <rlawn97@gmail.com>
2021-06-01 23:42:56 +0900

    make ui PostViewPage

--- petam/src/Components/Content.jsx
+++ petam/src/Components/Content.jsx
@@ -1,20 +1,16 @@
-import React from 'react'
-import './Content.css'
+import React from "react";
+import "./Content.css";
 
-function Content({children}){
-    const bodyStyle={
-        background:'white',
-        height:'auto',
-        minHeight:'77vh',
-        width:'90%',
-        padding:'10px 5px',
-        margin:'4% 5%',
-        borderRadius:'20px'
-    }
-    return(
-        <div style={bodyStyle}>
-            {children}
-        </div>
-    )
+function Content({ children }) {
+  const bodyStyle = {
+    background: "white",
+    height: "auto",
+    minHeight: "77vh",
+    width: "90%",
+    padding: "10px 5px",
+    margin: "4% 5%",
+    borderRadius: "20px",
+  };
+  return <div style={bodyStyle}>{children}</div>;
 }
-export default Content
+export default Content;
--- petam/src/Components/view/PostViewWriterDate.jsx
+++ petam/src/Components/view/PostViewWriterDate.jsx
@@ -1,50 +1,35 @@
 import React from "react";
 import "./PostView.css";
 
-const PostViewWriterDate = (props) => {
-  const { headersName, children } = props;
-  <div className="post-view">
-    {headersName.map((item, index) => {
-      return (
-        <td className="post-table-header-column" key={index}>
-          {item}
-        </td>
-      );
-    })}
-  </div>;
-  //   return (
-  //     <div>
-  //       <h6 className="post-view-writer-date">{writer}</h6>
-  //       <h6 className="post-view-writer-date">{date}</h6>
-  //     </div>
-  //   );
+// const PostViewWriterDate = (props) => {
+//   const { headersName, children } = props;
+//   <div className="post-view">
+//     {headersName.map((item, index) => {
+//       return <h6 key={index}>{item}</h6>;
+//     })}
+//   </div>;
+//   //   return (
+//   //     <div>
+//   //       <h6 className="post-view-writer-date">{writer}</h6>
+//   //       <h6 className="post-view-writer-date">{date}</h6>
+//   //     </div>
+//   //   );
+// };
+
+// export default PostViewWriterDate;
+
+const PostTable = (props) => {
+  const { writer, date, children } = props;
+
+  return (
+    // <table className="">
+    //   <thead>
+    <div className="">
+      <div className=" post-view-writer-date">
+        {writer} ⎢ {date}
+      </div>
+    </div>
+  );
 };
 
-export default PostViewWriterDate;
-
-// const PostTable = props => {
-//   const { headersName, children } = props;
-
-//   return (
-//     <table className="post-table">
-//       <thead>
-//         <tr>
-//           {
-//             headersName.map((item, index) => {
-//               return (
-//                 <td className="post-table-header-column" key={index}>{ item }</td>
-//               )
-//             })
-//           }
-//         </tr>
-//       </thead>
-//       <tbody>
-//         {
-//           children
-//         }
-//       </tbody>
-//     </table>
-//   )
-// }
-
-// export default PostTable;
+export default PostTable;
