6c2e8c762ec9481aa766b9492f2a506adbaaa625
ktmihs <yellowgreen423@gmail.com>
2021-06-02 16:05:29 +0900

    petam get api

--- petam/src/Pages/SearchContent.jsx
+++ petam/src/Pages/SearchContent.jsx
@@ -1,34 +1,20 @@
 import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import '../Components/Content.css'
+import Hospital from './Hospital'
 
-function SearchContent(){
-    const [info,setInfo]=useState({
-        name:'병원',
-        img:'/sev.jpg',
-        ph:'02-123-1004'
-    })
-    const bodyHospital={
-        display:'inline-block',
-        border:'5px solid #98B6E4',
-        textAlign:'center',
-        height:'190px',
-        width:'160px',
-        margin:'20px 20px 0 20px'
-    }
-    const imgStyle={
-        width:'150px',
-        height:'130px',
-        verticalAlign:'top'
-    }
-
+function SearchContent({info}){
+ 
     return(
+
         <Link to='/HospitalPage'>
-            <div style={bodyHospital}>
-                <img style={imgStyle} src={info.img}/>
-                {info.name}<br/>
-                tel: {info.ph}
-            </div>
+            {
+                info.map(item=>{
+                    return(
+                        <Hospital key={item.id} item={item}/>
+                    )
+                })
+        }
         </Link>
     )
 }
