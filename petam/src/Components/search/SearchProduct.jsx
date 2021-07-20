820719158833943e73d5545a394119b54f9f8d40
joo-ju <rlawn97@gmail.com>
2021-07-20 22:32:00 +0900

    제품 검색 구현

--- petam/src/Components/search/SearchProduct.jsx
+++ petam/src/Components/search/SearchProduct.jsx
@@ -3,16 +3,28 @@ import "../Content.css";
 import "./Search.css";
 
 function SearchProduct() {
+const [state, setState] = useState({
+    information: [],
+    keyword: '',
+  });
+  // const state = ;
+
+  const handleChange = e => {
+    setState({
+  keyword: e.target.value,
+})
+  }
+
   return (
     <form
       //   onSubmit={handleSubmit}
       className="headerContainer "
     >
       <input
-        placeholder="제품명..."
-        // value={searchWord}
-        // onChange={handleChange}
-        name="name"
+        placeholder="Search..."
+        // value={keyword}
+        onChange={handleChange}
+        // name="name"
         className="search "
       />
       <button type="submit" className="button search-button">
